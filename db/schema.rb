8c6eeca2b8f156cf2f1998a898e2b470ba936e42
Gheorghe Tarcea <tarcea@gmail.com>
2020-07-03 16:03:28 +0200

    logo changed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_02_080319) do
+ActiveRecord::Schema.define(version: 2020_07_02_200435) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -24,6 +24,8 @@ ActiveRecord::Schema.define(version: 2020_07_02_080319) do
     t.string "provider"
     t.string "uid"
     t.text "image"
+    t.string "website"
+    t.text "bio"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
